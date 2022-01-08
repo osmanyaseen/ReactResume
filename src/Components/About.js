300677b7eaf94bbc8e8f9f072dce309935c5d59b
oyaseen <osmanyaseen@live.com>
2022-01-08 14:23:14 -0700

    Personalized

--- src/Components/About.js
+++ src/Components/About.js
@@ -8,10 +8,8 @@ class About extends Component {
     const name = this.props.data.name;
     const profilepic = "images/" + this.props.data.image;
     const bio = this.props.data.bio;
-    const street = this.props.data.address.street;
     const city = this.props.data.address.city;
-    const state = this.props.data.address.state;
-    const zip = this.props.data.address.zip;
+    const province = this.props.data.address.province;
     const phone = this.props.data.phone;
     const email = this.props.data.email;
     const resumeDownload = this.props.data.resumedownload;
@@ -38,9 +36,7 @@ class About extends Component {
                     <span>{name}</span>
                     <br />
                     <span>
-                      {street}
-                      <br />
-                      {city} {state}, {zip}
+                      {city}, {province}
                     </span>
                     <br />
                     <span>{phone}</span>
